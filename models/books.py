060908741d95cab5932be2f6d512c1e3caa5a57a
kelleshh <shabieieie@gmail.com>
2026-02-09 16:55:01 +0300

    feat(models): add books model with constraints and indexes

---
+++ models/books.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import CheckConstraint, Index
+from database import Model
+
+class BooksModel(Model):
+    __tablename__ = 'books'
+    
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        init=False, # т.к. Model наследуется от MappedAsDataclass 
+    )
+    title: Mapped[str] = mapped_column(
+        nullable=False,
+    )
+    author: Mapped[str] = mapped_column(
+        nullable=False,
+    )
+    year: Mapped[int | None] = mapped_column(
+        nullable=True,
+    )
+    pages: Mapped[int | None] = mapped_column(
+        nullable=True,
+    )
+    is_read: Mapped[bool] = mapped_column(
+        default=False,
+        server_default='false', # на случай если создается не через ORM
+        nullable=False,
+    )
+    
+
+    __table_args__ = (
+        # Ограничение на релевантный год и положительное количество страниц
+        CheckConstraint(
+            '(year IS NULL) OR (year BETWEEN -2000 AND EXTRACT(YEAR FROM NOW()))',
+            name='ck_books_year_range',
+        ),
+        CheckConstraint(
+            '(pages IS NULL) OR (pages > 0)',
+            name='ck_pages_positive',
+        ),
+        # Индексы под частые запросы
+        Index('ix_books_author',  'author'),
+        Index('ix_books_is_read', 'is_read'),
+    )
